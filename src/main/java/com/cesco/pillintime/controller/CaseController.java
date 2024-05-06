1735fccf768e26cbc5f129e4cca1ad27d4ab5233
tkdldjstm0127@gmail.com <tkdldjstm0127@gmail.com>
2024-05-06 16:23:26 +0900

    [Fix] Entity Member 변수 이름 수정 및 CaseControllor url 수정

--- src/main/java/com/cesco/pillintime/controller/CaseController.java
+++ src/main/java/com/cesco/pillintime/controller/CaseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/case")
+@RequestMapping("/api/case")
 @RequiredArgsConstructor
 public class CaseController {
 
