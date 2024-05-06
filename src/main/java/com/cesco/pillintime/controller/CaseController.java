fc60d13dbb47fda4e4525357ba6c95397b46ae78
tkdldjstm0127@gmail.com <tkdldjstm0127@gmail.com>
2024-05-06 16:07:02 +0900

    [Feat] 약통 추가 API #24

---
+++ src/main/java/com/cesco/pillintime/controller/CaseController.java
@@ -0,0 +1,21 @@
+package com.cesco.pillintime.controller;
+
+import com.cesco.pillintime.service.CaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/case")
+@RequiredArgsConstructor
+public class CaseController {
+
+    private final CaseService caseService;
+
+    @PostMapping
+    public void createCase(@RequestBody String uuid) {
+        caseService.createCase(uuid);
+    }
+}
---
+++ src/main/java/com/cesco/pillintime/service/CaseService.java
@@ -0,0 +1,36 @@
+package com.cesco.pillintime.service;
+
+import com.cesco.pillintime.dto.MemberDto;
+import com.cesco.pillintime.entity.Case;
+import com.cesco.pillintime.entity.Member;
+import com.cesco.pillintime.exception.CustomException;
+import com.cesco.pillintime.exception.ErrorCode;
+import com.cesco.pillintime.repository.CaseRepository;
+import com.cesco.pillintime.repository.MemberRepository;
+import com.cesco.pillintime.util.JwtUtil;
+import com.cesco.pillintime.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CaseService {
+
+    private final CaseRepository caseRepository;
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+
+    public void createCase(String uuid){
+
+        Long id = SecurityUtil.getCurrentMemberId();
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        Case newCase = new Case();
+        newCase.setUuid(uuid);
+        newCase.setOwner_id(member);
+
+        caseRepository.save(newCase);
+    }
+}
