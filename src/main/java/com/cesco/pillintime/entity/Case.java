b6020bcf7d25473c0cdb924bc5f1f43f99ed8f5d
tkdldjstm0127@gmail.com <tkdldjstm0127@gmail.com>
2024-05-06 15:57:54 +0900

    [Feat] Case Entity 생성

---
+++ src/main/java/com/cesco/pillintime/entity/Case.java
@@ -0,0 +1,23 @@
+package com.cesco.pillintime.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Case {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String uuid;
+
+    @JoinColumn(name="memberId")
+    @ManyToOne
+    private Member owner_id;
+}
