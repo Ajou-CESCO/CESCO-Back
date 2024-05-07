a300caa150d6532e6355560ab341e591c12f6534
bigpie1367 <bigpie1367@gmail.com>
2024-05-07 16:00:33 +0900

    [Feat] 센서값 전송 기능 구현 #32

---
+++ src/main/generated/com/cesco/pillintime/mapper/RelationMapperImpl.java
@@ -0,0 +1,11 @@
+package com.cesco.pillintime.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-07T15:38:00+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 18.0.2 (Azul Systems, Inc.)"
+)
+public class RelationMapperImpl extends RelationMapper {
+}
