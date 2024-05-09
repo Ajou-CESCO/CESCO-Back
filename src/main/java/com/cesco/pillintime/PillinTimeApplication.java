e8ae59a2d95d81eec0ab156e1b634f6025f09b77
bigpie1367 <bigpie1367@gmail.com>
2024-05-09 18:37:59 +0900

    [Feat] 약품 검색 기능 구현 #40

--- src/main/java/com/cesco/pillintime/PillinTimeApplication.java
+++ src/main/java/com/cesco/pillintime/PillinTimeApplication.java
@@ -2,12 +2,22 @@ package com.cesco.pillintime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PillinTimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PillinTimeApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new MappingJackson2XmlHttpMessageConverter());
+		return restTemplate;
 	}
 
 }
