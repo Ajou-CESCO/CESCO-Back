0b2f385f472f1c70447c459ed78a108708cd615f
bigpie1367 <bigpie1367@gmail.com>
2024-05-15 17:59:26 +0900

    [Feat] 복용 기록 업데이트 기능 구현 및 날짜 데이터 리팩토링 #32

--- src/main/java/com/cesco/pillintime/cabinet/service/CabinetService.java
+++ src/main/java/com/cesco/pillintime/cabinet/service/CabinetService.java
@@ -3,6 +3,9 @@ package com.cesco.pillintime.cabinet.service;
 import com.cesco.pillintime.cabinet.dto.CabinetDto;
 import com.cesco.pillintime.cabinet.dto.SensorDto;
 import com.cesco.pillintime.cabinet.entity.Cabinet;
+import com.cesco.pillintime.log.entity.Log;
+import com.cesco.pillintime.log.entity.TakenStatus;
+import com.cesco.pillintime.log.repository.LogRepository;
 import com.cesco.pillintime.member.entity.Member;
 import com.cesco.pillintime.exception.CustomException;
 import com.cesco.pillintime.exception.ErrorCode;
@@ -12,12 +15,15 @@ import com.cesco.pillintime.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 @RequiredArgsConstructor
 public class CabinetService {
 
     private final CabinetRepository cabinetRepository;
     private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
 
     public void createCabinet(CabinetDto cabinetDto) {
         String serial = cabinetDto.getSerial();
@@ -44,9 +50,21 @@ public class CabinetService {
 
     public void getSensorData(SensorDto sensorDto) {
         String serial = sensorDto.getSerial();
-        int index = sensorDto.getIndex();
+        int sensorIndex = sensorDto.getIndex();
 
+        // Cabinet 및 Member 정보 가져오기
         Cabinet cabinet = cabinetRepository.findBySerial(serial)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CABINET));
+        Member owner = memberRepository.findByCabinet(cabinet)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_OWNER));
+
+        // 오늘의 로그 찾기
+        LocalDate today = LocalDate.now();
+        Log todayLog = logRepository.findByMemberAndPlannedAt(owner, today)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_LOG));
+
+        // 로그 상태 업데이트
+        todayLog.setTakenStatus(TakenStatus.COMPLETED);
+        logRepository.save(todayLog);
     }
 }
