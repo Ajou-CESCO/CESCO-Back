6c9e1efb17af641fe46fcef71df67d107ba8670c
bigpie1367 <bigpie1367@gmail.com>
2024-05-19 16:37:02 +0900

    [Feat] 전화번호 인증 API 구현 #72

--- src/main/java/com/cesco/pillintime/auth/controller/AuthController.java
+++ src/main/java/com/cesco/pillintime/auth/controller/AuthController.java
@@ -1,8 +1,9 @@
 package com.cesco.pillintime.auth.controller;
 
 import com.cesco.pillintime.auth.dto.LoginDto;
-import com.cesco.pillintime.dto.ResponseDto;
+import com.cesco.pillintime.auth.dto.SmsDto;
 import com.cesco.pillintime.auth.serivce.AuthService;
+import com.cesco.pillintime.dto.ResponseDto;
 import com.cesco.pillintime.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,5 +25,11 @@ public class AuthController {
     public ResponseEntity<ResponseDto> login(@RequestBody LoginDto loginDto) {
         String token = authService.login(loginDto);
         return ResponseUtil.makeResponse(200, "Success login", Map.of("access_token", token));
+    }
+
+    @PostMapping("/sms")
+    public ResponseEntity<ResponseDto> getSmsCode(@RequestBody SmsDto smsDto) {
+        String code = authService.getSmsCode(smsDto);
+        return ResponseUtil.makeResponse(200, "Success get code", Map.of("code", code));
     }
 }
