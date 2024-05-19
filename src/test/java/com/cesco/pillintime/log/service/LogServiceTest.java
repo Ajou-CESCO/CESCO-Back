fd644f8effb78d738f6ab4c70d0ab62d9c03a31a
kim hakjun <swkhj2022@ajou.ac.kr>
2024-05-20 00:23:16 +0900

    [Feat] plan, log 간략 구현

---
+++ src/test/java/com/cesco/pillintime/log/service/LogServiceTest.java
@@ -0,0 +1,79 @@
+package com.cesco.pillintime.log.service;
+
+import com.cesco.pillintime.log.dto.LogDto;
+import com.cesco.pillintime.log.entity.Log;
+import com.cesco.pillintime.log.repository.LogRepository;
+import com.cesco.pillintime.member.entity.Member;
+import com.cesco.pillintime.member.repository.MemberRepository;
+import com.cesco.pillintime.plan.repository.PlanRepository;
+import com.cesco.pillintime.relation.entity.Relation;
+import com.cesco.pillintime.relation.repository.RelationRepository;
+import com.cesco.pillintime.security.CustomUserDetails;
+import com.cesco.pillintime.util.SecurityUtil;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class LogServiceTest {
+    private final RelationRepository relationRepository = mock(RelationRepository.class);
+
+    @Mock
+    private LogRepository logRepository;
+    @Mock
+    private PlanRepository planRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private SecurityUtil securityUtil;
+    @InjectMocks
+    private LogService logService;
+
+    @Test
+    void createDoseLog() {
+    }
+
+    @Test
+    void getDoseLogByMemberId() {
+        // Given
+        Long targetId = 1L;
+        Member requestMember = mock(Member.class);
+        Member targetMember = mock(Member.class);
+        when(memberRepository.findById(targetId)).thenReturn(Optional.of(targetMember));
+
+        List<Relation> relationList = new ArrayList<>();
+        relationList.add(new Relation(requestMember,targetMember));
+        when(relationRepository.findByMember(requestMember)).thenReturn(Optional.of(relationList));
+
+        List<Log> logList = new ArrayList<>();
+        logList.add(0,any());
+        when(logRepository.findByMemberAndPlannedAt(targetMember,any())).thenReturn(Optional.of(logList));
+        
+        Authentication authentication = mock(Authentication.class);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        when(authentication.getName()).thenReturn("username");
+        when(authentication.getPrincipal()).thenReturn(mock(CustomUserDetails.class));
+        when(SecurityUtil.getCurrentMember()).thenReturn(Optional.of(requestMember));
+        
+        // When
+        List<LogDto> logDtoList = logService.getDoseLogByMemberId(targetId);
+
+        // Then
+        verify(memberRepository, times(1)).findById(targetId);
+        verify(relationRepository, times(1)).findByMember(requestMember);
+        verify(logRepository, times(1)).findByMemberAndPlannedAt(targetMember,any());
+    }
+
+    @Test
+    void updateDoseLogByCurrentTime() {
+    }
+}
---
+++ src/test/java/com/cesco/pillintime/medicine/service/MedicineServiceTest.java
@@ -0,0 +1,19 @@
+package com.cesco.pillintime.medicine.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MedicineServiceTest {
+
+    @Test
+    void getMedicineInfoByName() {
+    }
+
+    @Test
+    void getMedicineByMedicineId() {
+    }
+
+    @Test
+    void parseJsonResponse() {
+    }
+}
