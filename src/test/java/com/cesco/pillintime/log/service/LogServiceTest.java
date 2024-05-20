a6856bee994ae6dc24f188e89bfc0545cdb3e11e
kim hakjun <swkhj2022@ajou.ac.kr>
2024-05-20 19:05:29 +0900

    [Fit] merge 준비

--- src/test/java/com/cesco/pillintime/log/service/LogServiceTest.java
+++ src/test/java/com/cesco/pillintime/log/service/LogServiceTest.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
