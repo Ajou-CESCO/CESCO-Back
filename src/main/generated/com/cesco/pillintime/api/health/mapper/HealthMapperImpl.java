a8ee79b098587ed3b15a08a556fc6bb2cf2d2bd6
kim hakjun <swkhj2022@ajou.ac.kr>
2024-05-29 10:59:14 +0900

    [Fix] checkout용

---
+++ src/main/generated/com/cesco/pillintime/api/health/mapper/HealthMapperImpl.java
@@ -0,0 +1,30 @@
+package com.cesco.pillintime.api.health.mapper;
+
+import com.cesco.pillintime.api.health.dto.HealthDto;
+import com.cesco.pillintime.api.health.entity.Health;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-29T10:57:17+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
+)
+public class HealthMapperImpl implements HealthMapper {
+
+    @Override
+    public HealthDto toDto(Health health) {
+        if ( health == null ) {
+            return null;
+        }
+
+        HealthDto healthDto = new HealthDto();
+
+        healthDto.setId( health.getId() );
+        healthDto.setSteps( health.getSteps() );
+        healthDto.setCal( health.getCal() );
+        healthDto.setSleepTime( health.getSleepTime() );
+        healthDto.setLastUpLoadTime( health.getLastUpLoadTime() );
+
+        return healthDto;
+    }
+}
---
+++ src/main/generated/com/cesco/pillintime/api/relation/mapper/RelationMapperImpl.java
@@ -0,0 +1,11 @@
+package com.cesco.pillintime.api.relation.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-29T10:57:17+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
+)
+public class RelationMapperImpl extends RelationMapper {
+}
