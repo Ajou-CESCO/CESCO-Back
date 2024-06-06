bb8e9499276eeb18b6a8a8b23eeb035526e871bf
bigpie1367 <bigpie1367@gmail.com>
2024-06-06 15:27:12 +0900

    [Fix] 임시 버그 리포트 삭제

--- src/main/java/com/cesco/pillintime/api/bug/repository/BugRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cesco.pillintime.api.bug.repository;
-
-import com.cesco.pillintime.api.bug.entity.Bug;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BugRepository extends JpaRepository<Bug, Long> {
-}
