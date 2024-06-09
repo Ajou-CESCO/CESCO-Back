3135a80320302673e35c4e0903580e4b1d71b160
kim hakjun <swkhj2022@ajou.ac.kr>
2024-06-09 15:52:00 +0900

    [Feat] Auth test 일부 생성

--- src/test/java/com/cesco/pillintime/auth/serivce/AuthServiceTest.java
+++ src/test/java/com/cesco/pillintime/auth/serivce/AuthServiceTest.java
@@ -1,77 +1,116 @@
-//package com.cesco.pillintime.auth.serivce;
-//
-//import com.cesco.pillintime.api.auth.dto.LoginDto;
-//import com.cesco.pillintime.api.auth.serivce.AuthService;
-//import com.cesco.pillintime.exception.CustomException;
-//import com.cesco.pillintime.exception.ErrorCode;
-//import com.cesco.pillintime.api.member.entity.Member;
-//import com.cesco.pillintime.api.member.repository.MemberRepository;
-//import com.cesco.pillintime.security.JwtUtil;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.Optional;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.*;
-//import static org.mockito.Mockito.when;
-//
-//class AuthServiceTest {
-//
-//    private MemberRepository memberRepository;
-//    private JwtUtil jwtUtil;
-//    private AuthService authService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        memberRepository = mock(MemberRepository.class);
-//        jwtUtil = mock(JwtUtil.class);
-//        authService = new AuthService(memberRepository, jwtUtil);
-//    }
-//
-//    @Test
-//    void login_Success() {
-//        // Given
-//        Member member = mock(Member.class);
-//        LoginDto loginDto = new LoginDto();
-//        loginDto.setName("asdf");
-//        loginDto.setPhone("010-1234-5678");
-//        loginDto.setSsn("123456-7890123");
-//
-//        String extoken = "memo";
-//
-//        when(memberRepository.findByNameAndPhoneAndSsn(any(),any(),any())).thenReturn(Optional.of(member));
-//        when(jwtUtil.createAccessToken(any())).thenReturn(extoken);
-//
-//        // When
-//        String retoken = authService.login(loginDto);
-//
-//        // Then
-//        verify(memberRepository,times(1)).findByNameAndPhoneAndSsn(any(),any(),any());
-//        verify(jwtUtil,times(1)).createAccessToken(member);
-//        Assertions.assertEquals(extoken, retoken);
-//    }
-//
-//    @Test
-//    void login_NotFoundMember() {
-//        // Given
-//        Member member = mock(Member.class);
-//        LoginDto loginDto = new LoginDto();
-//        loginDto.setName("asdf");
-//        loginDto.setPhone("010-1234-5678");
-//        loginDto.setSsn("123456-7890123");
-//
-//        String extoken = "memo";
-//
-//        when(memberRepository.findByNameAndPhoneAndSsn(any(),any(),any())).thenReturn(Optional.empty());
-//        when(jwtUtil.createAccessToken(any())).thenReturn(extoken);
-//
-//        // When
-//        CustomException customException = Assertions.assertThrows(CustomException.class, () -> authService.login(loginDto));
-//
-//        // Then
-//        verify(memberRepository,times(1)).findByNameAndPhoneAndSsn(any(),any(),any());
-//        Assertions.assertEquals(ErrorCode.NOT_FOUND_USER, customException.getErrorCode());
-//    }
-//}
+package com.cesco.pillintime.api.auth.service;
+
+import com.cesco.pillintime.api.auth.dto.LoginDto;
+import com.cesco.pillintime.api.auth.dto.SmsDto;
+import com.cesco.pillintime.api.auth.serivce.AuthService;
+import com.cesco.pillintime.api.member.entity.Member;
+import com.cesco.pillintime.api.member.repository.MemberRepository;
+import com.cesco.pillintime.exception.CustomException;
+import com.cesco.pillintime.exception.ErrorCode;
+import com.cesco.pillintime.security.JwtUtil;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private DefaultMessageService defaultMessageService;
+
+    @InjectMocks
+    private AuthService authService;
+
+    private Member guardian;
+    @BeforeEach
+    void setUp() {
+        guardian = new Member();
+        guardian.setId(1L);
+        guardian.setName("guardian");
+        guardian.setSsn("123123-1");
+        guardian.setPhone("010-1234-1234");
+        guardian.setManager(false);
+
+        // Set up values for properties annotated with @Value
+        ReflectionTestUtils.setField(authService, "apiKey", "testApiKey");
+        ReflectionTestUtils.setField(authService, "secretKey", "testSecretKey");
+        ReflectionTestUtils.setField(authService, "from", "testFrom");
+    }
+
+    @Nested
+    class 로그인 {
+        @Test
+        void 로그인() {
+            // given
+            LoginDto loginDto = new LoginDto();
+            loginDto.setName(guardian.getName());
+            loginDto.setPhone(guardian.getPhone());
+            loginDto.setSsn(guardian.getSsn());
+
+            when(memberRepository.findByNameAndPhoneAndSsn(any(), any(), any())).thenReturn(Optional.of(guardian));
+            when(jwtUtil.createAccessToken(guardian)).thenReturn("testToken");
+
+            // when
+            String accessToken = authService.login(loginDto);
+
+            // then
+            assertEquals("testToken", accessToken);
+            verify(memberRepository, times(1)).findByNameAndPhoneAndSsn(loginDto.getName(), loginDto.getPhone(), loginDto.getSsn());
+            verify(jwtUtil, times(1)).createAccessToken(guardian);
+        }
+        @Test
+        void 회원가입하지_않은_사용자() {
+            // given
+            LoginDto loginDto = new LoginDto();
+            loginDto.setName(guardian.getName());
+            loginDto.setPhone(guardian.getPhone());
+            loginDto.setSsn(guardian.getSsn());
+
+            when(memberRepository.findByNameAndPhoneAndSsn(any(), any(), any())).thenReturn(Optional.empty());
+
+            // when
+            CustomException exception = assertThrows(CustomException.class, () -> {
+                authService.login(loginDto);
+            });
+            // then
+            assertEquals(ErrorCode.NOT_FOUND_USER, exception.getErrorCode());
+
+            verify(memberRepository, times(1)).findByNameAndPhoneAndSsn(loginDto.getName(), loginDto.getPhone(), loginDto.getSsn());
+            verify(jwtUtil, never()).createAccessToken(any());
+        }
+    }
+
+    @Nested
+    class 문자인증 {
+        @Test
+        void 문자인증() {
+
+        }
+        @Test
+        void 예외_서버에러() {
+
+        }
+    }
+}
